65bd4c049313b82113007f877bd57b256bceb771
Shivashankar-741 <shiva1894300@gmail.com>
2021-05-12 21:09:54 +0530

    changes

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import colors from "colors";
-import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import colors from 'colors';
+import dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
 
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("server side from loancraft app");
+app.get('/', (req, res) => {
+  res.send('server side from loancraft app');
 });
 
 const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`.inverse.green);
+  console.log(`App running up on port ${PORT}`.inverse.green);
 });
