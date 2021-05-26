9f203a13491bc3cb042634eb979ba8302ea51931
Shivashankar-741 <shiva1894300@gmail.com>
2021-05-26 20:59:14 +0530

    db connected to application

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import colors from 'colors';
 import dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json({ limit: '30mb', extended: true }));
-app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.json({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('server side from loancraft app');
+  res.send('LoanCraft API here');
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`App running up on port ${PORT}`.inverse.green);
-});
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
+  })
+  .catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify', false);
