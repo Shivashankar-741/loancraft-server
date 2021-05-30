34544a076a1dc267cc7878e094727432e8126752
Shivashankar-741 <shiva1894300@gmail.com>
2021-05-30 20:45:15 +0530

    auth implementation api's has done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import loanRoutes from './routes/loans.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 dotenv.config();
@@ -16,7 +17,7 @@ app.use(express.json({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/loans', loanRoutes);
-// app.use('/users', userRoutes);
+app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('LoanCraft API here');
--- models/user.js
+++ models/user.js
@@ -4,9 +4,9 @@ const userSchema = mongoose.Schema({
   name: { type: 'string', required: true },
   email: { type: 'string', required: true },
   password: { type: 'string', required: true },
-  id: { type: 'string', required: true },
+  id: { type: 'string' },
 });
 
-const user = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-export default user;
+export default User;
